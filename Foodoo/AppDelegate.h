5f1f256a01c91a7d5e176883bbd8938ede17ddf1
Toni Saarela <tn.saarela.dev@gmail.com>
2016-05-11 18:51:39 +0200

    initial commit for project files

---
+++ Foodoo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foodoo
+//
+//  Created by admin_user on 5/11/16.
+//  Copyright © 2016 WorldStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Foodoo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Foodoo
+//
+//  Created by admin_user on 5/11/16.
+//  Copyright © 2016 WorldStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
